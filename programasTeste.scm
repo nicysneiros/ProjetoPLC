cc235d172becd366bebe55decf60a3cd0e1e2d72
Rafael <rlcocin@gmail.com>
2013-02-26 17:22:13 -0300

    Adcionando programa teste numero 8

--- programasTeste.scm
+++ programasTeste.scm
@@ -1,9 +1,12 @@
+
+
+
 ; PROGRAMA 1 [Funções utilizadas: if/eqv?/list-comp/(/)]:
 ; (begin 
 ; 	(if (eqv? '(1 'a 3) '(1 'b 3)) (list-comp x (1 5 6 7 2 3 725 23 178) (* x 2) (eqv? (mod x 2) 0)) (/ 10 3))
 ; )
 
-; PROGRAMA 2 [Funções utilizadas: ]:
+; PROGRAMA 2 [Funções utilizadas:  ]:
 
 ; PROGRAMA 3 [Funções utilizadas: ]:
 
@@ -15,7 +18,10 @@
 
 ; PROGRAMA 7 [Funções utilizadas: ]:
 
-; PROGRAMA 8 [Funções utilizadas: ]:
+; PROGRAMA 8 [Funções utilizadas: if/and/>=/eqv?/length/cons/comment ]:
+;(begin 
+;	(if (and (>= 2 2) (eqv? (length (cons 'a  '(1 2 3 4 5))) 6)) (cons 'u '(h u u)) (* 2 5) (comment "Teste"))
+; )
 
 ; PROGRAMA 9 [Funções utilizadas: ]:
 
